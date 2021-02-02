65df7a56c8d348afc8a0e86e155c1d5269effa61
akurako <aleksejs.kurako@gmail.com>
2021-02-02 22:00:31 +0100

    fixed

--- src/Loader.java
+++ src/Loader.java
@@ -3,52 +3,55 @@ import java.util.Scanner;
 public class Loader {
 
     public static void main(String[] args) {
-        boolean main_cycle = true;
-        boolean inner_cycle;
+        int program_state = 1;
         int leftop = 0;
         int rightop = 0;
         char action = 0;
         Scanner in = new Scanner(System.in);
-        while (main_cycle == true) {
+        while (program_state >= 1) {
             System.out.println("Vvedite pervoe chislo");
             leftop = leftop + in.nextInt();
-            inner_cycle = true;
+            program_state = 2;
 
-            while (inner_cycle == true) {
+            while (program_state == 2) {
 
                 System.out.println("Chto budem delatj? +,-,*,/,c,q");
                 action = in.next().charAt(0);
-                if (action == 'c') {
-                    leftop = 0;
-                    break;
-                }
-                    else
-                        if (action == 'q') {
-                            inner_cycle = false;
-                            main_cycle = false;
-                            break;
-                        }
-                    System.out.println("Vvedite vtoroe chislo");
-                    rightop = in.nextInt();
 
-                    switch (action) {
-                        case '+':
-                            System.out.println(leftop = leftop + rightop);
-                            break;
-                        case '-':
-                            System.out.println(leftop = leftop - rightop);
-                            break;
-                        case '/':
-                            System.out.println(leftop = leftop / rightop);
-                            break;
-                        case '*':
-                            System.out.println(leftop = leftop * rightop);
-                            break;
-                        default:
-                            System.out.println("Ty vtiraesh mne kakuju-to dich!");
-                            inner_cycle = false;
+                switch (action) {
+                    case 'q':
+                        program_state = 0;
+                        break;
+                    case 'c':
+                        leftop = 0;
+                        program_state = 1;
+                        break;
+                    case '+', '-', '*', '/': {
+
+                        System.out.println("Vvedite vtoroe chislo");
+                        rightop = in.nextInt();
+
+                        switch (action) {
+                            case '+':
+                                System.out.println(leftop = leftop + rightop);
+                                break;
+                            case '-':
+                                System.out.println(leftop = leftop - rightop);
+                                break;
+                            case '/':
+                                System.out.println(leftop = leftop / rightop);
+                                break;
+                            case '*':
+                                System.out.println(leftop = leftop * rightop);
+                                break;
+                        }
+                        break;
                     }
+                    default:
+                        System.out.println("Ty vtiraesh mne kakuju-to dich!");
+                        program_state = 2;
                 }
             }
         }
     }
+}
